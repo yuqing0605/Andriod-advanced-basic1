e6a9dd2d2f62a9eb28a00979c45465b14a14f804
LinYuChhing <linyuching@gmail.com>
2021-11-05 20:07:38 +0800

    first upload

---
+++ basic1/app/src/main/java/com/example/basic1/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.basic1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void logingo(View view){
+        Button btn = findViewById(R.id.login);
+        btn.setText("logout");
+    }
+}
